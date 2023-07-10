d40b26e3c6b5b32d62d74ad436ec6e4745c31375
adi <you@example.com>
2023-07-10 22:35:15 +0800

    refactor: 升级相关依赖包

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 'use client';
-import { AspectRatio, Center, Stack, Heading } from '@chakra-ui/react';
-import Image from 'next/image';
+
+import { Box, Center, Heading, Text } from '@chakra-ui/react';
+import { BiMoon } from 'react-icons/bi';
 
 const Home = () => {
   return (
     <Center h="100vh">
-      <Stack align="center" spacing="8">
-        <AspectRatio ratio={283 / 64} w="200px">
-          <Image
-            src="/vercel.svg"
-            alt="vercel"
-            fill
-            sizes="(max-width: 768px) 100vw,
-          (max-width: 1200px) 50vw,
-          33vw"
-          />
-        </AspectRatio>
-
-        <Heading size="lg">NextJS-Typescript-Chakra-UI-Template</Heading>
-      </Stack>
+      <Heading size="lg">
+        <Box>
+          <Center>
+            <BiMoon />
+          </Center>
+        </Box>
+        <Text fontSize="sm">Next JS Chakra-UI Template</Text>
+      </Heading>
     </Center>
   );
 };
